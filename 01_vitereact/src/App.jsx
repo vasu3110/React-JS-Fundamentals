c0fe1e31c3ccc13f924ba6112c815bb10e70e548
Vasu <vasu22300@gmail.com>
2025-01-18 18:47:40 +0530

    Created my own custom react

--- 01_vitereact/src/App.jsx
+++ 01_vitereact/src/App.jsx
@@ -1,9 +1,11 @@
 import Chai from "./Chai"
 function App() {
+  const username="Chai aur code";
   return (
     <>
       <Chai/>
-      <h1>Hello</h1>
+      <h1>Hello {username}</h1>
+      <p>test Para</p>
     </>
   )
 }
--- 01_vitereact/src/main.jsx
+++ 01_vitereact/src/main.jsx
@@ -1,10 +1,38 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+function MyApp(){
+  return(
+    <div>
+      <h1>Custom App | chai</h1>
+    </div>
+  )
+}
+
+// const ReactElement={
+//   type:'a',
+//   props:{
+//     href:'https://google.com',
+//     target:'_blank'
+//   },
+//   children:'Click me to visit google'
+// }
+
+const anotherElement=(
+  <a href="https://google.com" target='_blank'>Visit Google</a>
 )
 
+const anotherUser="Chai aur React";
+
+const reactElement=React.createElement(
+  'a',
+  {href:'https://google.com',target:'_blank'},
+  'click me to visit google',
+  anotherElement,anotherUser
+)
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  reactElement
+)
